a642311e21f3ea2d42f6f93b1cb769e5a2aef4ab
Ted Spinks <ted.spinks@codefresh.io>
2022-07-14 14:00:13 -0400

    Update generate-change-vars.sh

--- generate-change-vars.sh
+++ generate-change-vars.sh
@@ -19,7 +19,7 @@ function choose_color() {
     # Sets output var COLOR_CHOICE
     declare -a COLOR_LIST=("LemonChiffon" "LightCyan" "LightBlue" \
     "LightGoldenRodYellow" "LightGrey" "Lavender" "HoneyDew")
-    COLOR_INDEX=$(($RANDOM % 7 + 1))
+    COLOR_INDEX=$(($RANDOM % 7))
     export COLOR_CHOICE=${COLOR_LIST[COLOR_INDEX]}
     cf_export COLOR_CHOICE || true
     # echo "Color choice: $COLOR_CHOICE"
@@ -28,7 +28,7 @@ function choose_color() {
 function choose_jira_issue() {
     # Sets output var JIRA_ISSUE
     declare -a ISSUE_LIST=("KPT-17" "KPT-18" "KPT-19" "KPT-20" "KPT-21" "KPT-22" "KPT-23" "KPT-24" "KPT-25" "KPT-26" "KPT-27" "KPT-28" "KPT-29")
-    ISSUE_INDEX=$(($RANDOM % 13 + 1))
+    ISSUE_INDEX=$(($RANDOM % 13))
     export JIRA_ISSUE=${ISSUE_LIST[ISSUE_INDEX]}
     export JIRA_ISSUE_PREFIX=KPT
     cf_export JIRA_ISSUE || true
@@ -39,7 +39,7 @@ function choose_commit_message() {
     # Sets output var COMMIT_MESSAGE
     declare -a MESSAGE_LIST=("Update style.css" "Update background color" \
         "Apply $COLOR_CHOICE color scheme" "Add highlights for pizzaz" "Code refactor" "Update dependencies")
-    MESSAGE_INDEX=$(($RANDOM % 6 + 1))
+    MESSAGE_INDEX=$(($RANDOM % 6))
     export COMMIT_MESSAGE=${MESSAGE_LIST[MESSAGE_INDEX]}
     cf_export COMMIT_MESSAGE || true
     # echo "Commit message: $COMMIT_MESSAGE"
